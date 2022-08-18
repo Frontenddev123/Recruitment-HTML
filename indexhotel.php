d2d547cb698bbe73c7385b6ece131c1d1f0428df
dhimo simoni <dhimo-simoni@hotmail.com>
2022-08-18 20:09:47 +0200

    add-small-changes

---
+++ indexhotel.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'C:\xampp\htdocs\panel\Recruitment-HTML\hotel\layouts\header.php';
+include 'C:\xampp\htdocs\panel\Recruitment-HTML\hotel\layouts\content.php';
+include 'C:\xampp\htdocs\panel\Recruitment-HTML\hotel\layouts\footer.php';
+
+
+
+?>
