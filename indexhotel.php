d036c236349bfc0b0c108ae4f493382893f70ea4
Frontenddev123 <vena.llanaj@outlook.com>
2022-08-19 11:58:20 +0200

    add-icons

--- indexhotel.php
+++ indexhotel.php
@@ -3,6 +3,7 @@
 include 'C:\xampp\htdocs\panel\hotel\layouts\header.php';
 include 'C:\xampp\htdocs\panel\hotel\layouts\menu.php';
 include 'C:\xampp\htdocs\panel\hotel\layouts\content.php';
+include 'C:\xampp\htdocs\panel\hotel\layouts\positionprocess.php';
 include 'C:\xampp\htdocs\panel\hotel\layouts\footer.php';
 
 
