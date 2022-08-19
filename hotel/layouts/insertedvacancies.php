84295e40382366a1ff52d1d653d6c50be246ac5e
Frontenddev123 <vena.llanaj@outlook.com>
2022-08-19 12:02:23 +0200

    rename-content-to-inserted-vacancies

---
+++ hotel/layouts/insertedvacancies.php
@@ -0,0 +1,265 @@
+<!--begin::Toolbar-->
+<div class="toolbar py-5 py-lg-5" id="kt_toolbar">
+	<!--begin::Container-->
+	<div id="kt_toolbar_container" class="container-xxl d-flex flex-stack flex-wrap">
+		<!--begin::Page title-->
+		<div class="page-title d-flex flex-column me-3">
+			<!--begin::Title-->
+			<h1 class="d-flex text-dark fw-bold my-1 fs-3">Dashboard</h1>
+			<!--end::Title-->
+			<!--begin::Breadcrumb-->
+			<p>Welcome back</p>
+					
+				<!--end::Breadcrumb-->
+			</div>
+			<!--end::Page title-->
+			<!--begin::Actions-->
+			<div class="d-flex align-items-center py-2 py-md-1">
+				<!--begin::Wrapper-->
+			<div class="me-3">							
+			<!--end::Input group-->
+			<!--begin::Actions-->
+			<div class="d-flex justify-content-end">
+				
+			<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_1">
+			Advertise a job</button>
+
+	<div class="modal fade" tabindex="-1" id="kt_modal_1">
+    <div class="modal-dialog modal-lg ">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title">Insert Vacancies</h3>
+
+                <!--begin::Close-->
+                <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal" aria-label="Close">
+                    <span class="svg-icon svg-icon-1">X</span>
+                </div>
+                <!--end::Close-->
+            </div>
+
+            <div class="modal-body">
+		        <form >
+                     <div class="row"> 
+
+				
+                 <!-- 2 column grid layout with text inputs for the first and last names -->
+     
+                   <!-- Text input -->
+                 
+                   <div class="col-md-6">
+                   <div class="form-outline mb-4">
+	         	  <input type="text" name="jobPosition" class="form-control" placeholder="jobPosition">
+                     <label class="form-label" for="form6Example3"></label>
+                   </div>  
+                   </div>  
+                     
+
+                   <!-- Text input -->
+                   <div class="col-md-6">
+                   <div class="form-outline mb-4">
+	         	  <input type="text" name="employeesNumber" class="form-control"
+                    placeholder="employeesNumber">
+                    <label class="form-label" for="form6Example4"></label>
+                   </div>
+                   </div>
+   
+ 
+
+                    <!-- 2 column grid layout with text inputs for the first and last names -->
+                  
+                     <div class="col-md-6">
+                       <div class="form-outline">
+	         		  <input type="text" name="jobType" class="form-control" placeholder="jobType">
+                         <label class="form-label" for="form6Example1"></label>
+                       </div>
+                     </div>
+
+                     <div class="col-md-6">
+                       <div class="form-outline">
+	         		  <input type="text" name="salary" class="form-control" placeholder="salary">
+
+                         <label class="form-label" for="form6Example2"></label>
+                       </div>
+                     </div>
+     
+                   
+                 
+                   <!-- Email input -->
+  
+                      <div class="col-md-6">
+                       <div class="form-outline ">
+	         	       <input type="text" name="experience" class="form-control" placeholder="experience">
+                     <label class="form-label" for="form6Example5"></label>
+                   </div>
+                 </div>
+                       
+
+                   <!-- Country input -->
+      
+                   <div class="col-md-6">
+                   <div class="form-outline ">
+	         	  <input type="text" name="dayOff" class="form-control" placeholder="dayOff">
+
+                     <label class="form-label" for="form6Example6"></label>
+                   </div>
+                   </div>
+
+                     
+
+                   <!-- Addres Line1 input -->
+     
+                      <div class="col-md-6">
+                   <div class="form-outline ">
+	         	  <input type="text" name="lunchBreak" class="form-control" placeholder="lunch Break">
+
+                     <label class="form-label" for="form6Example5"></label>
+                   </div>
+                   </div>
+
+
+                    <!-- Addres Line2 input -->
+                 <div class="row">
+                   <div class="form-outline ">
+                   <textarea class="form-control" id="form6Example7" rows="4" ame="comment" class="form-control" placeholder="comment"></textarea>
+	
+                   </div>
+                   
+                 </div>  
+				 </div>
+           </div>
+
+       </form>
+          
+            <div class="modal-footer">
+			<button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</div>
+
+
+	<!--end::Actions-->
+	</div>
+		<!--end::Form-->
+		</div>
+			<!--end::Menu 1-->
+				<!--end::Menu-->				
+			</div>
+			<!--end::Wrapper-->
+			<!--begin::Button-->
+			
+			<!--end::Button-->
+		</div>
+		<!--end::Actions-->
+	</div>
+	<!--end::Container-->
+</div>
+		
+
+</div>
+
+
+
+<div id="kt_content_container" class="d-flex flex-column-fluid align-items-start container-xxl">
+ 		<!--begin::Post-->
+		 <div class="content flex-row-fluid" id="kt_content">
+							
+							<div class="row gy-5 g-xl-10">
+								
+								<!--begin::Col-->
+								<div class="col-xl-12 mb-5 mb-xl-10">
+									<!--begin::Table Widget 4-->
+									<div class="card card-flush h-xl-100">
+										<!--begin::Card header-->
+										<div class="card-header pt-7">
+											<!--begin::Title-->
+											<h3 class="card-title align-items-start flex-column">
+												<span class="card-label fw-bold text-gray-800">Inserted Vacancies</span>
+												
+											</h3>
+											<!--end::Title-->
+											<!--begin::Actions-->
+											<div class="card-toolbar">
+												<!--begin::Filters-->
+												<div class="d-flex flex-stack flex-wrap gap-4">
+													
+												
+													<!--begin::Search-->
+													<div class="position-relative my-1">
+														<!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
+														<span class="svg-icon svg-icon-2 position-absolute top-50 translate-middle-y ms-4">
+															<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+																<rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="currentColor" />
+																<path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="currentColor" />
+															</svg>
+														</span>
+														<!--end::Svg Icon-->
+														<input type="text" data-kt-table-widget-4="search" class="form-control w-150px fs-7 ps-12" placeholder="Search"  />
+													</div>
+													<!--end::Search-->
+												</div>
+												<!--begin::Filters-->
+											</div>
+											<!--end::Actions-->
+										</div>
+										<!--end::Card header-->
+										<!--begin::Card body-->
+										<div class="card-body pt-2">
+										<div class="table-responsive">
+                                             <table class="table table-striped gy-7 gs-7">
+                                              <thead>
+                                               <tr class="fw-semibold fs-6 text-gray-800 border-bottom border-gray-200">
+                                                <th>Order ID</th>
+                                                <th>Start Date</th>
+                                                <th>End Date</th>
+                                                <th>Job Position</th>
+                                                <th>State</th>
+                                                <th>Number Of students</th>
+												<th> <img src="assets/media/icons/check.png" alt="check">
+
+												</th>
+                                               </tr>
+                                              </thead>
+                                              <tbody>
+                                               <tr>
+                                                <td>1</td>
+                                                <td>04/11/2022</td>
+                                                <td>04/12/2022</td>
+                                                <td>Waiter</td> 
+                                                <td><img src="assets/media/icons/flag-Al.png" alt="Flag"></td>
+                                                <td>30</td>
+												
+												<td><img src="assets/media/icons/hamburg-menu-2-16.png" alt="hamburg-icons"></td>
+                                               </tr>
+											   <tr>
+                                                <td>2</td>
+                                                <td>04/11/2022</td>
+                                                <td>04/12/2022</td>
+                                                <td>Waiter</td> 
+                                                <td><img src="assets/media/icons/flag-Al.png" alt="Flag"></td>
+                                                <td>30</td>
+												<td><img src="assets/media/icons/hamburg-menu-2-16.png" alt="hamburg-icons"></td>
+                                               </tr>
+                                              </tbody>
+                                             </table>
+                                            </div>
+                                            										
+										</div>
+										<!--end::Card body-->
+									</div>
+									<!--end::Table Widget 4-->
+								</div>
+								<!--end::Col-->
+							</div>
+							<!--end::Row-->
+							
+							<!--end::Row-->
+						</div>
+						<!--end::Post-->				
+					</div>
+
+
+					
