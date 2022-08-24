d561bb426972efb3d77b23e7dde978f897614fea
vena <vena.llanaj@gmail.com>
2022-08-24 07:30:35 +0200

    add-changes

--- hotel/layouts/header.php
+++ hotel/layouts/header.php
@@ -40,37 +40,24 @@
 					<!--begin::Header-->
 					<div id="kt_header" class="header" data-kt-sticky="true" data-kt-sticky-name="header" data-kt-sticky-offset="{default: '200px', lg: '300px'}">
 						<!--begin::Container-->
-						<div class="container-xxl d-flex flex-grow-1 flex-stack">
+						<div class="container-fluid d-flex flex-grow-1 flex-stack">
 							<!--begin::Header Logo-->
 							<div class="d-flex align-items-center me-5">
-								<!--begin::Heaeder menu toggle-->
-								<div class="d-lg-none btn btn-icon btn-active-color-primary w-30px h-30px ms-n2 me-3" id="kt_header_menu_toggle">
-									<!--begin::Svg Icon | path: icons/duotune/abstract/abs015.svg-->
-									<span class="svg-icon svg-icon-2">
-										<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-											<path d="M21 7H3C2.4 7 2 6.6 2 6V4C2 3.4 2.4 3 3 3H21C21.6 3 22 3.4 22 4V6C22 6.6 21.6 7 21 7Z" fill="currentColor" />
-											<path opacity="0.3" d="M21 14H3C2.4 14 2 13.6 2 13V11C2 10.4 2.4 10 3 10H21C21.6 10 22 10.4 22 11V13C22 13.6 21.6 14 21 14ZM22 20V18C22 17.4 21.6 17 21 17H3C2.4 17 2 17.4 2 18V20C2 20.6 2.4 21 3 21H21C21.6 21 22 20.6 22 20Z" fill="currentColor" />
-										</svg>
-									</span>
-									<!--end::Svg Icon-->
-								</div>
-								<!--end::Heaeder menu toggle-->
-								
-								<img alt="Logo" src="assets/media/logos/logo_header.png" class=" h-20px h-lg-30px" />
-
-								
+							<img alt="Logo" src="assets/media/logos/logo_header.png" class=" h-20px h-lg-30px" />
+			
 							</div>
 							<!--end::Header Logo-->
 							<!--begin::Topbar-->
-							<div class="d-flex align-items-center">
 							
 							<div class="d-flex align-items-center">
+							
+							<div class="d-flex align-items-center ms-lg-4">
 						     <ul class="navbar ms-md-auto gap-2 list-unstyled">       	 
                                    <li class="nav-item rounded">
                                      <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-house-fill me-4 fa-lg"></i>Home</a>
                                    </li>
 	                      	     <li class="nav-item rounded">
-                                     <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-people me-4  fa-lg"></i>Students in Intership</a>
+                                     <a class="nav-link active" aria-current="page" href="http://localhost/Recruitment-HTML/test.php"><i class="bi bi-people me-4  fa-lg"></i>Students in Intership</a>
                                    </li>
 	                      	     <li class="nav-item rounded">
                                      <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-clock me-4  fa-lg"></i>Students Waiting</a>
@@ -89,9 +76,9 @@
 							<div class="d-flex align-items-center flex-shrink-0">
 							
 							<!--begin::Theme mode-->
-								<div class="d-flex align-items-center ms-3 ms-lg-4">
+							<div class="d-flex align-items-center ms-3 ms-lg-4">
 									<!--begin::Menu toggle-->
-									<a href="#" class="btn btn-icon btn-color-gray-700 btn-active-color-primary btn-outline btn-outline-secondary btn-active-bg-light w-30px h-30px w-lg-40px h-lg-40px" data-kt-menu-trigger="{default:'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+									<a href="#" class="btn btn-icon btn-color-gray-700 btn-active-color-primary btn-outline btn-active-bg-light w-30px h-30px w-lg-40px h-lg-40px" data-kt-menu-trigger="{default:'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
 										<!--begin::Svg Icon | path: icons/duotune/general/gen060.svg-->
 										<span class="svg-icon theme-light-show svg-icon-2">
 											<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -183,6 +170,8 @@
 									</div>
 									<!--end::Menu-->
 								</div>
+							
+								
 								<!--end::Theme mode-->
 								<!--begin::User-->
 								<div class="d-flex align-items-center ms-3 ms-lg-4" id="kt_header_user_menu_toggle">
---
+++ tablestudentintership.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'C:\xampp\htdocs\\Recruitment-HTML\hotel\layouts\header.php';
+
+include 'C:\xampp\htdocs\Recruitment-HTML\hotel\layouts\footer.php';
+
+
+
+?>
