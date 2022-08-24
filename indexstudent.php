96b5587370602731b9e72a5cb1ef9279784b593c
Frontenddev123 <vena.llanaj@outlook.com>
2022-08-24 16:59:36 +0200

    add

--- indexstudent.php
+++ indexstudent.php
@@ -4,6 +4,7 @@ include 'C:\xampp\htdocs\panel\hotel\layouts\header.php';
 
 
 include 'C:\xampp\htdocs\panel\student\layouts\studentintership.php';
+include 'C:\xampp\htdocs\panel\student\layouts\registerstudent.php';
 
 include 'C:\xampp\htdocs\panel\hotel\layouts\footer.php';
 
