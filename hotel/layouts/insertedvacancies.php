d96027a887e85d15298cc025f25200e790431710
Frontenddev123 <vena.llanaj@outlook.com>
2022-08-25 15:06:54 +0200

    made-some-fixed-at-hotel

--- hotel/layouts/insertedvacancies.php
+++ hotel/layouts/insertedvacancies.php
@@ -70,7 +70,7 @@
                               <td><img src="assets/media/icons/flag-Al.png" alt="Flag"></td>
                               <td>30</td>
 							
-							<td><img src="assets/media/icons/hamburg-menu-2-16.png" alt="hamburg-icons" data-bs-toggle="modal" data-bs-target="#kt_modal_2"></td>
+							<td><img src="assets/media/icons/hamburg-menu-2-16.png" alt="hamburg-icons" data-bs-toggle="modal" data-bs-target="#positionprocess"></td>
                              </tr>
 						   <tr>
                               <td>2</td>
@@ -81,105 +81,95 @@
                               <td>30</td>
 						
 
-							<td><img src="assets/media/icons/hamburg-menu-2-16.png" alt="hamburg-icons" data-bs-toggle="modal" data-bs-target="#kt_modal_2"></td>
+							<td><img src="assets/media/icons/hamburg-menu-2-16.png" alt="hamburg-icons" data-bs-toggle="modal" data-bs-target="#positionprocess"></td>
                              </tr>
                             </tbody>
                         </table>
 	 
                     </div>           
                 </div>
+                
+                <div class="modal fade" id="positionprocess" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 
-             <div class="modal fade" tabindex="-1" id="kt_modal_2">
-               <div class="modal-dialog modal-xl modal-dialog-centered">
-                   <div class="modal-content">
-                       <div class="modal-header">
-                        <div class="container">
-                       <div class="row align-items-center">
-                            <div class="col-md-4">
-                            <h1>ID: A1001</h1>
-                            </div>
-                           
-                             <div class="col-md-4"></div>
-                            
-                            <div class="col-md-4">
-                              	<!--begin::Search-->
-							      	<div class="position-relative my-1">
-					      				<!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
-					      				<span class="svg-icon svg-icon-2 position-absolute top-50 translate-middle-y ms-4">
-				      					
-					      					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-					      						<rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="currentColor" />
-					      						<path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="currentColor" />
-					      					</svg>
-					      				</span>
-					      				<!--end::Svg Icon-->
-					      				<input type="text"  class="form-control w-150px fs-7 ps-12" placeholder="Search"  id="myInput1" onkeyup="myFunction1()" />
-                                          <div id="not-found-1"></div>
-                                 
-								    </div>
-								<!--end::Search-->
-                            </div>
-                          </div>                        
+                     <div class="modal-dialog modal-dialog-centered  modal-xl">
+                        
+                       <!-- Start Modal content -->
+                       <div class="modal-content">                     
 
-                         
+                            <div class="modal-header">                     
+
+                               <h2 class="modal-title" id="exampleModalLabel">Modal title</h2>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
                           
-                      
-                       </div>
-                       </div>
+                           <!-- Start Modal Body -->
+                              <div class="modal-body">
+                                <div class="container">
+                                   <div class="table-resposive">
+                                        <div class="table-wrapper">
+                                           <div class="table-title">
+                                               <div class="row">
+                                                   <div class="col-md-6">
+                                                        <h1>Blu Hotel    </h1>
+                                                   </div>
+                                                   <div class="col-md-6">
+                                                       <div class="search-box">
+                                                           <div class="input-group">
+                                                           <input type="text" id="search" class="form-control" placeholder="Search">
+                                                          
+                                                           </div>
+                                                       </div>
+                                                                            
 
-                       <div class="modal-body">
-                       <div class="table-responsive">
-                           <table class="table table-striped gy-7 gs-7">
-                            <thead>
-                             <tr class="fw-semibold fs-6 text-gray-800 border-bottom border-gray-200">
-                              <th>First Name</th>
-                              <th>Last Name</th>
-                              <th>CV</th>
-                              <th>Availability</th>
-                              <th>Start date</th>
-                              <th>End date</th>
-                              <th class="text-end">  <img src="assets/media/icons/check.png" alt="">
+                                                   </div>
+                                               </div>
+                                           </div>
+                                         <div class="table-responsive">
+                                       <table class="table table-striped gy-7 gs-7 mt-10">
+                                       <thead>
+                                       <tr class="fw-semibold fs-6 text-gray-800 border-bottom border-gray-200">
+                                          <th>First Name</th>
+                                          <th>Last Name</th>
+                                          <th>CV</th>
+                                          <th>Job Position</th>
+                                          <th>Start date</th>
+                                          <th>End date</th>
+                                          <th class="text-end">  <img src="assets/media/icons/check.png" alt="check">
+                                             
+                                         </tr>
+                                     </thead>
+                                     <tbody>
+                                         <tr>
+                                         <td>Tiger Nixon</td>
+                                         <td>System Architect</td>
+                                         <td>Pdf</td>
+                                         <td>Waiter</td>
+                                         <td>20/04/22</td>
+                                         <td>20/07/22</td>  
+                                         <td><i class="bi bi-person fs-2x"></i></td>
+                                         <td> <a href="#" class="btn btn-danger btn-sm">Accept</a></td>
+                                         <td> <a href="#" class="btn btn-danger btn-sm">Decline</a></td>                                         
+                                         </tr>                     
 
-                              </th>
-                           
-                             </tr>
-                            </thead>
-                            <tbody>
-                             <tr>
-                              <td>Tiger Nixon</td>
-                              <td>System Architect</td>
-                              <td>Edinburgh</td>
-                              <td>61</td>
-                              <td>2011/04/25</td>
-                              <td>$320,800</td>  
-                              <!-- <td>  <i class="bi bi-person "></i></td> -->
-                              <td><i class="bi bi-person fs-2x"></i></td>
-                              <td>  <a href="#" class="btn btn-danger btn-sm">Accept</a></td>
-                              <td>  <a href="#" class="btn btn-danger btn-sm">Decline</a></td>
-                            
-                             </tr>
-                             <tr>
-                              <td>Garrett Winters</td>
-                              <td>Accountant</td>
-                              <td>Tokyo</td>
-                              <td>63</td>
-                              <td>2011/07/25</td>
-                              <td>$170,750</td>
-                              <td>  <i class="bi bi-person fs-2x"></i></td>
-                              <td>  <a href="#" class="btn btn-danger btn-sm">Accept</a></td>
-                              <td>  <a href="#" class="btn btn-danger btn-sm">Decline</a></td>
-                             </tr>
-                            </tbody>
-                           </table>
-                       </div>
-           		        
-                </div>      
+                                    </tbody>
+                                </table>
+                           </div>
+                      </div>
+                     </div>
+                     </div>                     
+
+                     </div>
+                     </div>
+                     </div>
+                     </div>
+
+          
+
             </div>
-
-       </div>
-     </div>
-  </div>
+         </div>
+    </div>
   <script type="text/javascript" src="assets/js/tables.js"></script>
+</div>
 </div>
                 
 		
