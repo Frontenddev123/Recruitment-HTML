3f280b21d11b41c2b2917ab47a916c525a352373
Frontenddev123 <vena.llanaj@outlook.com>
2022-08-31 10:09:33 +0200

    removed-file

--- student/layouts/studentintership.php
+++ student/layouts/studentintership.php
@@ -82,7 +82,7 @@
                 <!-- Modal -->
                 <div class="modal fade" id="reviewmodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 
-                    <div class="modal-dialog modal-dialog-centered  modal-xl">
+                    <div class="modal-dialog modal-dialog-centered  ">
 
                         <div class="modal-content">
 
@@ -97,11 +97,12 @@
                              <div class="modal-body"> 
                                  <div class="container">
 
-                                     <div class="row">                                
+                                                                  
 
-                                         <div class="col-md-12">                                
-
-                                            <div class="row">                                
+                                            <div class="row">   
+                                                <div class="col-md-6"></div>  
+                                                <div class="col-md-6">Raiting</div>   
+                                                <hr>                           
 
                                                  <div class="col-md-6">                                
 
@@ -109,9 +110,8 @@
                                                      <hr>
                                                  </div>                                
 
-                                                 <div class="col-md-6">
-                                                            
-                                                   <div class="rating ">
+                                                 <div class="col-md-1">
+                                                    <div class="rating ">
                                                          <!--begin::Reset rating-->
                                                          <label class="  fw-bold btn-sm rating-label me-3" for="kt_rating_input_0" >
                                                              
@@ -126,51 +126,94 @@
                                                                                                               </svg></span>
                                                          </label>
                                                          <input class="rating-input" name="rating" value="1" type="radio" id="kt_rating_input_1"/>
-                                                         <!--end::Star 1-->                                
-                                                         </p>
-                                                         <!--begin::Star 2-->
-                                                         <label class="rating-label" for="kt_rating_input_2">
+                                                         <!--end::Star 1--> 
+                                                   </div>
+                                                                                   
+                                                </div> 
+                                                <div class="col-md-1">
+                                                    <div class="rating ">
+                                                         <!--begin::Reset rating-->
+                                                         <label class="  fw-bold btn-sm rating-label me-3" for="kt_rating_input_0" >
+                                                             
+                                                         </label>
+                                                         <input class="rating-input" name="rating" value="0" checked type="radio" id="kt_rating_input_0"/>
+                                                         <!--end::Reset rating-->                                
+                                                          <p class="star1"> 
+                                                         <!--begin::Star 1-->
+                                                         <label class="rating-label" for="kt_rating_input_1" class="pa-8">
                                                              <span class="svg-icon svg-icon-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
                                                                                                                 <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                                                                                                               </svg></span>
                                                          </label>
-                                                         <input class="rating-input" name="rating" value="2" type="radio" id="kt_rating_input_2"/>
-                                                         <!--end::Star 2-->                                
-
-                                                         <!--begin::Star 3-->
-                                                         <label class="rating-label" for="kt_rating_input_3">
+                                                         <input class="rating-input" name="rating" value="1" type="radio" id="kt_rating_input_1"/>
+                                                         <!--end::Star 1--> 
+                                                   </div>
+                                                                                   
+                                                </div> 
+                                                <div class="col-md-1">
+                                                    <div class="rating ">
+                                                         <!--begin::Reset rating-->
+                                                         <label class="  fw-bold btn-sm rating-label me-3" for="kt_rating_input_0" >
+                                                             
+                                                         </label>
+                                                         <input class="rating-input" name="rating" value="0" checked type="radio" id="kt_rating_input_0"/>
+                                                         <!--end::Reset rating-->                                
+                                                          <p class="star1"> 
+                                                         <!--begin::Star 1-->
+                                                         <label class="rating-label" for="kt_rating_input_1" class="pa-8">
                                                              <span class="svg-icon svg-icon-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
                                                                                                                 <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                                                                                                               </svg></span>
                                                          </label>
-                                                         <input class="rating-input" name="rating" value="3" type="radio" id="kt_rating_input_3"/>
-                                                         <!--end::Star 3-->                                
-
-                                                         <!--begin::Star 4-->
-                                                         <label class="rating-label" for="kt_rating_input_4">
+                                                         <input class="rating-input" name="rating" value="1" type="radio" id="kt_rating_input_1"/>
+                                                         <!--end::Star 1--> 
+                                                   </div>
+                                                                                   
+                                                </div> 
+                                                <div class="col-md-1">
+                                                    <div class="rating ">
+                                                         <!--begin::Reset rating-->
+                                                         <label class="  fw-bold btn-sm rating-label me-3" for="kt_rating_input_0" >
+                                                             
+                                                         </label>
+                                                         <input class="rating-input" name="rating" value="0" checked type="radio" id="kt_rating_input_0"/>
+                                                         <!--end::Reset rating-->                                
+                                                          <p class="star1"> 
+                                                         <!--begin::Star 1-->
+                                                         <label class="rating-label" for="kt_rating_input_1" class="pa-8">
                                                              <span class="svg-icon svg-icon-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
                                                                                                                 <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                                                                                                               </svg></span>
                                                          </label>
-                                                         <input class="rating-input" name="rating" value="4" type="radio" id="kt_rating_input_4"/>
-                                                         <!--end::Star 4-->                                
-
-                                                         <!--begin::Star 5-->
-                                                         <label class="rating-label" for="kt_rating_input_5">
+                                                         <input class="rating-input" name="rating" value="1" type="radio" id="kt_rating_input_1"/>
+                                                         <!--end::Star 1--> 
+                                                   </div>
+                                                                                   
+                                                </div> 
+                                                <div class="col-md-1">
+                                                    <div class="rating ">
+                                                         <!--begin::Reset rating-->
+                                                         <label class="  fw-bold btn-sm rating-label me-3" for="kt_rating_input_0" >
+                                                             
+                                                         </label>
+                                                         <input class="rating-input" name="rating" value="0" checked type="radio" id="kt_rating_input_0"/>
+                                                         <!--end::Reset rating-->                                
+                                                          <p class="star1"> 
+                                                         <!--begin::Star 1-->
+                                                         <label class="rating-label" for="kt_rating_input_1" class="pa-8">
                                                              <span class="svg-icon svg-icon-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
                                                                                                                 <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                                                                                                               </svg></span>
                                                          </label>
-                                                         <input class="rating-input" name="rating" value="5" type="radio" id="kt_rating_input_5"/>
-                                                         <!--end::Star 5-->
-                                                    </div>          
-                                                    </div> 
+                                                         <input class="rating-input" name="rating" value="1" type="radio" id="kt_rating_input_1"/>
+                                                         <!--end::Star 1--> 
+                                                   </div>
+                                                                                   
+                                                </div> 
 
-                                                   </div>  
+                                            </div>  
 
-                                         </div>     
-                                                                   
-                                     </div>
+                                       
 
                                  </div>
 
