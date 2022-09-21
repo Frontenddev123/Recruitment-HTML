e160fcc0fcfc7957e79483a5359068c099567a8c
Vena Llanaj <dhimo-simoni@hotmail.com>
2022-09-22 00:55:50 +0200

    adds

--- test1.php
+++ test1.php
@@ -1,276 +1,212 @@
+@extends('student.layouts.master')
+@section('content')
 <!DOCTYPE html>
-<!-- Coding By Danish Laeeq - patreon.com/danishlaeeq -->
-<html lang="en" dir="ltr">
-
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <title>Custom Select Menu in JavaScript | Danish Laeeq</title>
-  <link rel="stylesheet" href="style.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- Iconscout Link For Icons -->
-  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-  <style>
-    /* Import Google Font - Poppins */
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap");
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Poppins", sans-serif;
-}
-body {
-  background: #0077cd;
-}
-::selection {
-  color: #fff;
-  background: #4285f4;
-}
-.wrapper {
-  width: 370px;
-  margin: 85px auto 0;
-}
-.select-btn,
-li {
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-}
-.select-btn {
-  height: 65px;
-  padding: 0 20px;
-  font-size: 22px;
-  background: #fff;
-  border-radius: 7px;
-  justify-content: space-between;
-  box-shadow: 0 10px 25px rgba(0, 0, 0, 0.1);
-}
-.select-btn i {
-  font-size: 31px;
-  transition: transform 0.3s linear;
-}
-.wrapper.active .select-btn i {
-  transform: rotate(-180deg);
-}
-.content {
-  display: none;
-  padding: 20px;
-  margin-top: 15px;
-  background: #fff;
-  border-radius: 7px;
-  box-shadow: 0 10px 25px rgba(0, 0, 0, 0.1);
-}
-.wrapper.active .content {
-  display: block;
-}
-.content .search {
-  position: relative;
-}
-.search i {
-  top: 50%;
-  left: 15px;
-  color: #999;
-  font-size: 20px;
-  pointer-events: none;
-  transform: translateY(-50%);
-  position: absolute;
-}
-.search input {
-  height: 50px;
-  width: 100%;
-  outline: none;
-  font-size: 17px;
-  border-radius: 5px;
-  padding: 0 20px 0 43px;
-  border: 1px solid #b3b3b3;
-}
-.search input:focus {
-  padding-left: 42px;
-  border: 2px solid #4285f4;
-}
-.search input::placeholder {
-  color: #bfbfbf;
-}
-.content .options {
-  margin-top: 10px;
-  max-height: 250px;
-  overflow-y: auto;
-  padding-right: 7px;
-}
-.options::-webkit-scrollbar {
-  width: 7px;
-}
-.options::-webkit-scrollbar-track {
-  background: #f1f1f1;
-  border-radius: 25px;
-}
-.options::-webkit-scrollbar-thumb {
-  background: #ccc;
-  border-radius: 25px;
-}
-.options::-webkit-scrollbar-thumb:hover {
-  background: #b3b3b3;
-}
-.options li {
-  height: 50px;
-  padding: 0 13px;
-  font-size: 21px;
-}
-.options li:hover,
-li.selected {
-  border-radius: 5px;
-  background: #f2f2f2;
-}
-
-/* Support */
-.support-box {
-  top: 2rem;
-  position: relative;
-  bottom: 0;
-  text-align: center;
-  display: block;
-}
-.b-btn {
-  color: black;
-  text-decoration: none;
-  font-weight: bold;
-}
-.b-btn.patreon i {
-  color: orange;
-}
-.b-btn:hover {
-  text-decoration: none;
-  font-weight: bold;
-}
-.b-btn i {
-  font-size: 20px;
-  color: yellow;
-  margin-top: 2rem;
-}
-
-  </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-
 <body>
-  <div class="wrapper">
-    <div class="select-btn">
-      <span>Select Country</span>
-      <i class="uil uil-angle-down"></i>
-    </div>
-    <div class="content">
-      <div class="search">
-        <i class="uil uil-search"></i>
-        <input spellcheck="false" type="text" placeholder="Search">
-      </div>
-      <ul class="options"></ul>
-    </div>
-  </div>
+   <!--begin::Container-->
+	<div id="kt_content_container" class="d-flex flex-column-fluid align-items-start container-xxl">
+		<!--begin::Post-->
+		<div class="content flex-row-fluid" id="kt_content">
+			<!--begin::Navbar-->
+			<div class="card mb-5 mb-xxl-8">
+				<div class="card-body pt-9 pb-0">
+					<!--begin::Details-->
+					<div class="d-flex flex-wrap flex-sm-nowrap">
+						<!--begin: Pic-->
+						<div class="me-7 mb-4">
+							<div class="symbol symbol-100px symbol-lg-160px symbol-fixed position-relative">
+								<img src="{{ asset('uploads/' . Auth::guard('student')->user()->profileFoto) }}">
+							</div>
+						</div>
+						<!--end::Pic-->
+						<!--begin::Info-->
+						<div class="flex-grow-1">
+							<!--begin::Title-->
+							<div class="d-flex justify-content-between align-items-start flex-wrap mb-2">
+								<!--begin::User-->
+								<div class="d-flex flex-column">
+									<!--begin::Name-->
+									<div class="d-flex align-items-center mb-2">
+										<a href="#" class="text-gray-900 text-hover-primary fs-2 fw-bold me-1">{{ Auth::guard('student')->user()->firstName }} {{ Auth::guard('student')->user()->lastName }}</a>
+										<a href="#">
+											<!--begin::Svg Icon | path: icons/duotune/general/gen026.svg-->
+											<span class="svg-icon svg-icon-1 svg-icon-primary">
+												<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
+													<path d="M10.0813 3.7242C10.8849 2.16438 13.1151 2.16438 13.9187 3.7242V3.7242C14.4016 4.66147 15.4909 5.1127 16.4951 4.79139V4.79139C18.1663 4.25668 19.7433 5.83365 19.2086 7.50485V7.50485C18.8873 8.50905 19.3385 9.59842 20.2758 10.0813V10.0813C21.8356 10.8849 21.8356 13.1151 20.2758 13.9187V13.9187C19.3385 14.4016 18.8873 15.491 19.2086 16.4951V16.4951C19.7433 18.1663 18.1663 19.7433 16.4951 19.2086V19.2086C15.491 18.8873 14.4016 19.3385 13.9187 20.2758V20.2758C13.1151 21.8356 10.8849 21.8356 10.0813 20.2758V20.2758C9.59842 19.3385 8.50905 18.8873 7.50485 19.2086V19.2086C5.83365 19.7433 4.25668 18.1663 4.79139 16.4951V16.4951C5.1127 15.491 4.66147 14.4016 3.7242 13.9187V13.9187C2.16438 13.1151 2.16438 10.8849 3.7242 10.0813V10.0813C4.66147 9.59842 5.1127 8.50905 4.79139 7.50485V7.50485C4.25668 5.83365 5.83365 4.25668 7.50485 4.79139V4.79139C8.50905 5.1127 9.59842 4.66147 10.0813 3.7242V3.7242Z" fill="currentColor" />
+													<path d="M14.8563 9.1903C15.0606 8.94984 15.3771 8.9385 15.6175 9.14289C15.858 9.34728 15.8229 9.66433 15.6185 9.9048L11.863 14.6558C11.6554 14.9001 11.2876 14.9258 11.048 14.7128L8.47656 12.4271C8.24068 12.2174 8.21944 11.8563 8.42911 11.6204C8.63877 11.3845 8.99996 11.3633 9.23583 11.5729L11.3706 13.4705L14.8563 9.1903Z" fill="white" />
+												</svg>
+											</span>
+											<!--end::Svg Icon-->
+										</a>
+									</div>
+									<!--end::Name-->
+                                      	<!--begin::Info-->
+									<div class="d-flex flex-wrap fw-semibold fs-6 mb-4 pe-2">
+										<a href="#" class="d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2">
+										<!--begin::Svg Icon | path: icons/duotune/communication/com006.svg-->
+										<span class="svg-icon svg-icon-4 me-1">
+											<svg xmlns="http://www.w3.org/2000/svg"width="24" height="24" fill="currentColor" class="bi bi-telephone-minus-fill" viewBox="0 0 16 16">
+                                              <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/>
+                                            </svg>
+                                        </span>
+										<!--end::Svg Icon-->  {{ Auth::guard('student')->user()->adressLine }}</a>
+										<a href="#" class="d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2">
+										<!--begin::Svg Icon | path: icons/duotune/general/gen018.svg-->
+										<span class="svg-icon svg-icon-4 me-1">
+											<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+												<path opacity="0.3" d="M18.0624 15.3453L13.1624 20.7453C12.5624 21.4453 11.5624 21.4453 10.9624 20.7453L6.06242 15.3453C4.56242 13.6453 3.76242 11.4453 4.06242 8.94534C4.56242 5.34534 7.46242 2.44534 11.0624 2.04534C15.8624 1.54534 19.9624 5.24534 19.9624 9.94534C20.0624 12.0453 19.2624 13.9453 18.0624 15.3453Z" fill="currentColor" />
+												<path d="M12.0624 13.0453C13.7193 13.0453 15.0624 11.7022 15.0624 10.0453C15.0624 8.38849 13.7193 7.04535 12.0624 7.04535C10.4056 7.04535 9.06241 8.38849 9.06241 10.0453C9.06241 11.7022 10.4056 13.0453 12.0624 13.0453Z" fill="currentColor" />
+											</svg>
+										</span>
+										<!--end::Svg Icon-->  {{ Auth::guard('student')->user()->phoneNumber }}</a>
+										<a href="#" class="d-flex align-items-center text-gray-400 text-hover-primary mb-2">
+										<!--begin::Svg Icon | path: icons/duotune/communication/com011.svg-->
+										<span class="svg-icon svg-icon-4 me-1">
+											<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+												<path opacity="0.3" d="M21 19H3C2.4 19 2 18.6 2 18V6C2 5.4 2.4 5 3 5H21C21.6 5 22 5.4 22 6V18C22 18.6 21.6 19 21 19Z" fill="currentColor" />
+												<path d="M21 5H2.99999C2.69999 5 2.49999 5.10005 2.29999 5.30005L11.2 13.3C11.7 13.7 12.4 13.7 12.8 13.3L21.7 5.30005C21.5 5.10005 21.3 5 21 5Z" fill="currentColor" />
+											</svg>
+										</span>
+										<!--end::Svg Icon-->  {{ Auth::guard('student')->user()->email }}</a>
+									</div>
+									<!--end::Info-->
+									<!--begin::Info-->
+									<div class="d-flex flex-wrap fw-semibold fs-6 mb-4 pe-2">
+										<a href="#" class="d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2">
+										<!--begin::Svg Icon | path: icons/duotune/communication/com006.svg-->
+										<span class="svg-icon svg-icon-4 me-1">
+											<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+												<path opacity="0.3" d="M16.5 9C16.5 13.125 13.125 16.5 9 16.5C4.875 16.5 1.5 13.125 1.5 9C1.5 4.875 4.875 1.5 9 1.5C13.125 1.5 16.5 4.875 16.5 9Z" fill="currentColor" />
+												<path d="M9 16.5C10.95 16.5 12.75 15.75 14.025 14.55C13.425 12.675 11.4 11.25 9 11.25C6.6 11.25 4.57499 12.675 3.97499 14.55C5.24999 15.75 7.05 16.5 9 16.5Z" fill="currentColor" />
+												<rect x="7" y="6" width="4" height="4" rx="2" fill="currentColor" />
+											</svg>
+										</span>
+										<!--end::Svg Icon--> {{ Auth::guard('student')->user()->education }}</a>
+										<a href="#" class="d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2">
+										<!--begin::Svg Icon | path: icons/duotune/general/gen018.svg-->
+										<span class="svg-icon svg-icon-4 me-1">
+												<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-building" viewBox="0 0 16 16">
+                                              <path fill-rule="evenodd" d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"/>
+                                              <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"/>
+                                            </svg>
+										</span>
+										<!--end::Svg Icon--> {{ Auth::guard('student')->user()->university }}</a>
+										<a href="#" class="d-flex align-items-center text-gray-400 text-hover-primary mb-2">
+										<!--begin::Svg Icon | path: icons/duotune/communication/com011.svg-->
+										<span class="svg-icon svg-icon-4 me-1">
+											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-building" viewBox="0 0 16 16">
+                                              <path fill-rule="evenodd" d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"/>
+                                              <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"/>
+                                            </svg>                                            
+										</span>
+										<!--end::Svg Icon-->{{ Auth::guard('student')->user()->branch}}</a>
+									</div>
+									<!--end::Info-->
+                                  
+										
+								</div>
+								<!--end::User-->
+          				
+							<!--begin: video-->
+						    <div class="mb-4">
+						    	<div class="symbol symbol-100px symbol-lg-160px symbol-fixed position-relative">
+						    		<video width="100%" height="auto" controls
+                                    src="{{ URL::asset('uploads/' . Auth::guard('student')->user()->video) }}"> 
+                                   </video>
+						    	</div>
+						    </div>
+						<!--end::video-->
+								
+							</div>
+							<!--end::Title-->
+							
+						</div>
+						<!--end::Info-->
+					</div>
+					<!--end::Details-->
+					<!--begin::Navs-->
+					<ul class="nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-5 fw-bold">
+						<!--begin::Nav item-->
+						<li class="nav-item mt-2">
+							<a class="nav-link text-active-primary ms-0 me-10 py-5 active" href="../../demo11/dist/pages/user-profile/overview.html">Overview</a>
+						</li>
+						<!--end::Nav item-->
+						<!--begin::Nav item-->
+						<li class="nav-item mt-2">
+							<a class="nav-link text-active-primary ms-0 me-10 py-5" href="../../demo11/dist/pages/user-profile/projects.html">Projects</a>
+						</li>
+						<!--end::Nav item-->
+						<!--begin::Nav item-->
+						<li class="nav-item mt-2">
+							<a class="nav-link text-active-primary ms-0 me-10 py-5" href="../../demo11/dist/pages/user-profile/campaigns.html">Campaigns</a>
+						</li>
+						<!--end::Nav item-->
+						<!--begin::Nav item-->
+						<li class="nav-item mt-2">
+							<a class="nav-link text-active-primary ms-0 me-10 py-5" href="../../demo11/dist/pages/user-profile/documents.html">Documents</a>
+						</li>
+						<!--end::Nav item-->
+						<!--begin::Nav item-->
+						<li class="nav-item mt-2">
+							<a class="nav-link text-active-primary ms-0 me-10 py-5" href="../../demo11/dist/pages/user-profile/followers.html">Followers</a>
+						</li>
+						<!--end::Nav item-->
+						<!--begin::Nav item-->
+						<li class="nav-item mt-2">
+							<a class="nav-link text-active-primary ms-0 me-10 py-5" href="../../demo11/dist/pages/user-profile/activity.html">Activity</a>
+						</li>
+						<!--end::Nav item-->
+					</ul>
+					<!--begin::Navs-->
+				</div>
+			</div>
+			<!--end::Navbar-->
+           <div class="card">
+               <div class="card-body">
+                   <div class="card-title"><h3>More details</h3></div>
+                  <div class="row">
+                    <div class="col-md-6 border-right">
+                        <div class="p-3 py-5">
+                           
+                          
+                            <div class="row mt-3">
+                                <div class="col-md-4 mb-5"><label class="fw-semibold text-muted">Nid</label></div>
+                                <div class="col-md-4 mb-5"><span class="fw-semibold text-gray-800 fs-6">{{ Auth::guard('student')->user()->nid }}</span></div>
+                                
+                            </div>
+                            
+                        </div>
+                    </div>
+                   
+                  </div>
+              </div>
+            </div>
+     	</div>
+		<!--end::Post--> 
+ 	</div>
+<!--end::Container--> 
 
-  <!--   Support Section -->
-  <div class="support-box">
-    <a class="b-btn" target="blank" href="https://www.buymeacoffee.com/danishlaeeq">
-      &nbsp;<i class="fa fa-coffee"></i>&nbsp;&nbsp;Buy Me a Coffee
-    </a>
-    &nbsp;&nbsp;-&nbsp;&nbsp;
-    <a class="b-btn patreon" target="blank" href="https://www.patreon.com/danishlaeeq">
-      <i class="fab fa-patreon"></i>&nbsp;&nbsp;Become Patreon
-    </a>
-  </div>
-  </div>
 
-  <script>
-    const wrapper = document.querySelector(".wrapper"),
-  selectBtn = wrapper.querySelector(".select-btn"),
-  searchInp = wrapper.querySelector("input"),
-  options = wrapper.querySelector(".options");
 
-let countries = [
-  "Afghanistan",
-  "Algeria",
-  "Argentina",
-  "Australia",
-  "Bangladesh",
-  "Belgium",
-  "Bhutan",
-  "Brazil",
-  "Canada",
-  "China",
-  "Denmark",
-  "Ethiopia",
-  "Finland",
-  "France",
-  "Germany",
-  "Hungary",
-  "Iceland",
-  "India",
-  "Indonesia",
-  "Iran",
-  "Italy",
-  "Japan",
-  "Malaysia",
-  "Maldives",
-  "Mexico",
-  "Morocco",
-  "Nepal",
-  "Netherlands",
-  "Nigeria",
-  "Norway",
-  "Pakistan",
-  "Peru",
-  "Russia",
-  "Romania",
-  "South Africa",
-  "Spain",
-  "Sri Lanka",
-  "Sweden",
-  "Switzerland",
-  "Thailand",
-  "Turkey",
-  "Uganda",
-  "Ukraine",
-  "United States",
-  "United Kingdom",
-  "Vietnam"
-];
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 
-function addCountry(selectedCountry) {
-  options.innerHTML = "";
-  countries.forEach((country) => {
-    let isSelected = country == selectedCountry ? "selected" : "";
-    let li = `<li onclick="updateName(this)" class="${isSelected}">${country}</li>`;
-    options.insertAdjacentHTML("beforeend", li);
-  });
-}
-addCountry();
+    <script>
+        $(document).ready(function() {
 
-function updateName(selectedLi) {
-  searchInp.value = "";
-  addCountry(selectedLi.innerText);
-  wrapper.classList.remove("active");
-  selectBtn.firstElementChild.innerText = selectedLi.innerText;
-}
+            $('#button').click(function(e) {
+                $('.map').fadeToggle();
+            });
 
-searchInp.addEventListener("keyup", () => {
-  let arr = [];
-  let searchWord = searchInp.value.toLowerCase();
-  arr = countries
-    .filter((data) => {
-      return data.toLowerCase().startsWith(searchWord);
-    })
-    .map((data) => {
-      let isSelected =
-        data == selectBtn.firstElementChild.innerText ? "selected" : "";
-      return `<li onclick="updateName(this)" class="${isSelected}">${data}</li>`;
-    })
-    .join("");
-  options.innerHTML = arr
-    ? arr
-    : `<p style="margin-top: 10px;">Oops! Country not found</p>`;
-});
-
-selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
-
-  </script>
-
-</body>
-
+        });
+    </script>
+    </body>
 </html>
+@endsection
